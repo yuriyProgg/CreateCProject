5a25d3c5fc8e09b79a84332c383ca2aac8ab7f3a
Yuri Masalov <masalov.09@bk.ru>
2025-03-02 16:01:58 +0600

    Обновлены типы в module.h

--- module.h
+++ module.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "cJSON.h"
+
 char new_module(char module_name[64]);
 void get_include_path(cJSON *json, char *path[256], char *cwd[256]);
 void get_src_path(cJSON *json, char *path[256], char *cwd[256]);
