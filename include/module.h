9d3b4ecb666859758f487f42a49566174508dbc0
Yuri Masalov <masalov.09@bk.ru>
2025-07-07 16:12:41 +0600

    Рефакторинг module: добавлены функции get_lang_ext и get_header_ext, улучшена обработка путей

--- include/module.h
+++ include/module.h
@@ -2,6 +2,8 @@
 
 #include "cJSON.h"
 
+#define PATH_SIZE 256
+
 char new_module(const char *module_name);
 void get_include_dir(const char *buffer, char **path);
 void get_src_dir(const char *buffer, char **path);
--- src/module.c
+++ src/module.c
@@ -42,6 +42,42 @@ void get_src_dir(const char *buffer, char **path)
   cJSON_Delete(json);
 }
 
+void get_lang_ext(const char *buffer, char **path)
+{
+  cJSON *json = cJSON_Parse(buffer);
+  if (!json)
+  {
+    fprintf(stderr, "Ошибка: Не удалось распарсить JSON\n");
+    return;
+  }
+
+  cJSON *lang_ext = cJSON_GetObjectItem(json, "lang_ext");
+  if (cJSON_IsString(lang_ext) && lang_ext->valuestring != NULL)
+    *path = strdup(lang_ext->valuestring); // Копируем строку
+  else
+    fprintf(stderr, "Ошибка: 'lang_ext' не найден или не является строкой\n");
+
+  cJSON_Delete(json);
+}
+
+void get_header_ext(const char *buffer, char **path)
+{
+  cJSON *json = cJSON_Parse(buffer);
+  if (!json)
+  {
+    fprintf(stderr, "Ошибка: Не удалось распарсить JSON\n");
+    return;
+  }
+
+  cJSON *header_ext = cJSON_GetObjectItem(json, "header_ext");
+  if (cJSON_IsString(header_ext) && header_ext->valuestring != NULL)
+    *path = strdup(header_ext->valuestring); // Копируем строку
+  else
+    fprintf(stderr, "Ошибка: 'header_ext' не найден или не является строкой\n");
+
+  cJSON_Delete(json);
+}
+
 char new_module(const char *module_name)
 {
   char cwd[256];
@@ -51,7 +87,7 @@ char new_module(const char *module_name)
     return 2;
   }
 
-  char json_path[512];
+  char json_path[PATH_SIZE * 2];
   snprintf(json_path, sizeof(json_path), "%s/ccp.json", cwd);
 
   FILE *config_file = fopen(json_path, "r");
@@ -61,7 +97,7 @@ char new_module(const char *module_name)
     return 2;
   }
 
-  char buffer[512];
+  char buffer[PATH_SIZE];
   size_t bytes_read = fread(buffer, 1, sizeof(buffer) - 1, config_file);
   fclose(config_file);
   buffer[bytes_read] = '\0'; // Завершаем строку
@@ -73,95 +109,46 @@ char new_module(const char *module_name)
     return 2;
   }
 
-  cJSON *language = cJSON_GetObjectItem(json, "language");
-  if (cJSON_IsString(language) && language->valuestring != NULL)
+  char *src_dir = NULL, *include_dir = NULL, *lang_ext = NULL, *header_ext = NULL;
+  char cfile_path[PATH_SIZE], hfile_path[PATH_SIZE];
+
+  get_src_dir(buffer, &src_dir);
+  get_include_dir(buffer, &include_dir);
+  get_lang_ext(buffer, &lang_ext);
+  get_header_ext(buffer, &header_ext);
+
+  if (!src_dir || !include_dir || !lang_ext || !header_ext)
+    return 1;
+
+  if ((size_t)snprintf(cfile_path, sizeof(cfile_path), "%s/%s/%s.%s", cwd, src_dir, module_name, lang_ext) >= sizeof(cfile_path))
   {
-    char *include_dir = NULL;
-    char include_path[257];
-    get_include_dir(buffer, &include_dir);
-    if (include_dir)
-    {
-      snprintf(include_path, sizeof(include_path), "%s/%s", cwd, include_dir);
-      free(include_dir);
-    }
-    else
-    {
-      fprintf(stderr, "Ошибка: Директория 'include' не найдена\n");
-      cJSON_Delete(json);
-      return 3;
-    }
-
-    char *src_dir = NULL;
-    char src_path[257];
-    get_src_dir(buffer, &src_dir);
-    if (src_dir)
-    {
-      snprintf(src_path, sizeof(src_path), "%s/%s", cwd, src_dir);
-      free(src_dir);
-    }
-    else
-    {
-      fprintf(stderr, "Ошибка: Директория 'src' не найдена\n");
-      cJSON_Delete(json);
-      return 3;
-    }
-
-    if (!directory_exists(include_path) || !directory_exists(src_path))
-    {
-      fprintf(stderr, "Ошибка: Директории 'include' или 'src' не существуют\n");
-      cJSON_Delete(json);
-      return 3;
-    }
-
-    if (strcmp(language->valuestring, "cpp") == 0)
-    {
-      char cpp_path[512], hpp_path[512];
-      snprintf(cpp_path, sizeof(cpp_path), "%s/%s.cpp", src_path, module_name);
-      snprintf(hpp_path, sizeof(hpp_path), "%s/%s.hpp", include_path, module_name);
-
-      FILE *cpp_file = fopen(cpp_path, "w");
-      FILE *hpp_file = fopen(hpp_path, "w");
-      if (!cpp_file || !hpp_file)
-      {
-        perror("Ошибка: Не удалось создать файлы");
-        cJSON_Delete(json);
-        return 2;
-      }
-
-      fprintf(cpp_file, "#include \"%s.hpp\"\n", module_name);
-      fprintf(hpp_file, "#pragma once\n");
-
-      fclose(cpp_file);
-      fclose(hpp_file);
-    }
-    else
-    {
-      char c_path[512], h_path[512];
-      snprintf(c_path, sizeof(c_path), "%s/%s.c", src_path, module_name);
-      snprintf(h_path, sizeof(h_path), "%s/%s.h", include_path, module_name);
-
-      FILE *c_file = fopen(c_path, "w");
-      FILE *h_file = fopen(h_path, "w");
-      if (!c_file || !h_file)
-      {
-        perror("Ошибка: Не удалось создать файлы");
-        cJSON_Delete(json);
-        return 2;
-      }
-
-      fprintf(c_file, "#include \"%s.h\"\n", module_name);
-      fprintf(h_file, "#pragma once\n");
-
-      fclose(c_file);
-      fclose(h_file);
-    }
-  }
-  else
-  {
-    fprintf(stderr, "Ошибка: 'language' не найден или не является строкой\n");
-    cJSON_Delete(json);
+    fprintf(stderr, "Слишком длинный путь\n");
     return 1;
   }
+
+  FILE *cfile = fopen(cfile_path, "w");
+  if (!cfile)
+  {
+    perror("Ошибка: Не удалось создать файл");
+    return 2;
+  }
+  fprintf(cfile, "#include \"%s.%s\"\n", module_name, header_ext);
+  fclose(cfile);
+
+  if ((size_t)snprintf(hfile_path, sizeof(hfile_path), "%s/%s/%s.%s", cwd, include_dir, module_name, header_ext) >= sizeof(hfile_path))
+  {
+    fprintf(stderr, "Слишком длинный путь\n");
+    return 1;
+  }
+
+  FILE *hfile = fopen(hfile_path, "w");
+  if (!hfile)
+  {
+    perror("Ошибка: Не удалось создать файл");
+    return 2;
+  }
+  fprintf(hfile, "#pragma once\n");
+  fclose(hfile);
 
   cJSON_Delete(json);
   return 0;
